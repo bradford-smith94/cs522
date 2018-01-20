683b9473ad812200265fb3096a783a495d5a9c50
Bradford Smith <bsmith8@stevens.edu>
2018-01-19 21:05:34 -0500

    No longer include files matching .gitignore rule
    This updates the submit script to remove files matching the .gitignore
    rules, as well as the assignment rubric and spec files.

--- A1/submit.sh
+++ A1/submit.sh
@@ -5,18 +5,38 @@
 
 AUTHOR='Bradford_Smith'
 ASSIGNMENT='assignment1'
-ARCHIVE="$AUTHOR-$ASSIGNMENT.zip"
+ARCHIVE="${AUTHOR}_$ASSIGNMENT.zip"
 TMPDIR=$(mktemp -d)
+PREFIX="$TMPDIR/$AUTHOR"
 
-mkdir -p "$TMPDIR/$AUTHOR"
-echo "Copying everything to $TMPDIR/$AUTHOR"
-cp -r ./* "$TMPDIR/$AUTHOR"
+mkdir -p "$PREFIX"
+echo "Copying everything to $PREFIX"
+cp -r . "$PREFIX"
 
+if [ -f "$PREFIX/.gitignore" ]; then
+    echo "Removing excludes using .gitignore"
+    while read -r arg; do
+        if [[ "$arg" == /* ]]; then
+            #shellcheck disable=SC2206
+            dirs=($PREFIX/*$arg $PREFIX/.$arg)
+            #fail if PREFIX is empty to avoid system destruction (SC2115)
+            rm -r "${dirs[@]}" 2>/dev/null
+        else
+            #fail if PREFIX is empty to avoid system destruction (SC2115)
+            rm -r "${PREFIX:?}/$arg" 2>/dev/null
+        fi
+    done < "$TMPDIR/$AUTHOR/.gitignore"
+fi
+
+echo "Removing assignment specific excludes"
+rm "${PREFIX:?}/cs522_${ASSIGNMENT}_rubric.pdf"
+rm "${PREFIX:?}/cs522_${ASSIGNMENT}_spec.pdf"
+
+#need to enter temp dir for zip to execute
 echo "Entering $TMPDIR"
 #shellcheck disable=SC2164
 pushd "$TMPDIR"
 
-#TODO: remove excludes
 echo "Creating archive"
 zip -r "$ARCHIVE" "$AUTHOR"
 
